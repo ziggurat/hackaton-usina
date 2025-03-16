fa7904b389f87086e1b11cb9283f69169a795b01
Manuel <manuelcorrales@gmail.com>
2025-03-15 22:58:12 -0300

    Upload file to aws and return the url to the response.

--- router/main.py
+++ router/main.py
@@ -1,6 +1,10 @@
 import tempfile
 from typing import Annotated
 
+import boto3
+from boto3.s3.transfer import S3UploadFailedError
+from botocore.exceptions import ClientError, NoCredentialsError
+
 import aiofiles
 import uvicorn
 from fastapi import FastAPI, File, UploadFile, Response
@@ -75,12 +79,15 @@ async def create_upload_file(file: UploadFile, response: Response):
     agent_text_response = process(transcript)
     agent_audio_response = text_to_speech(agent_text_response)
 
+    # Upload to S3 and get the file URL
+    audio_file_url = upload_file_to_s3(agent_audio_response, "s3-hackaton-usina")
+
     # Return JSON response with file URL and other data
     allow_origin = response.headers.get("Access-Control-Allow-Origin")
     return {
         "agent": agent,
         "text_response": agent_text_response,
-        "audio_response_url": f"/download/{agent_audio_response.split('/')[-1]}",
+        "audio_response_url": audio_file_url,  # Use the URL returned from S3
         "Access-Control-Allow-Origin": allow_origin  # Assuming you have a download route
     }
 
@@ -94,6 +101,29 @@ def process(transcript):
 async def download_file(filename: str):
     file_path = f"/path/to/temp/directory/{filename}"  # Adjust the path accordingly
     return FileResponse(file_path, media_type='audio/mpeg')
+
+def upload_file_to_s3(file_name: str, bucket: str, object_name: str = None):
+    # If S3 object_name was not specified, use file_name
+    if object_name is None:
+        object_name = file_name
+
+    # Create an S3 client
+    s3_client = boto3.client('s3')
+
+    try:
+        # Upload the file
+        s3_client.upload_file(file_name, bucket, object_name)
+        print(f"File {file_name} uploaded to {bucket}/{object_name}")
+
+        # Construct the full URL of the uploaded file
+        file_url = f"https://{bucket}.s3.us-east-1.amazonaws.com/{object_name}"        
+        return file_url  # Return the full URL
+    except FileNotFoundError:
+        print(f"The file {file_name} was not found")
+    except NoCredentialsError:
+        print("Credentials not available")
+    except ClientError as e:
+        print(f"An error occurred: {e}")
 
 if __name__ == "__main__":
     uvicorn.run(app, host="0.0.0.0", port=8000)
